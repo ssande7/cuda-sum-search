4b930c581f5d499a0aad12b94d9aa519b4136b11
ssande7 <>
2022-05-03 21:48:25 +1000

    Updated test parameters

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 t=$1
-max_mem=1 #GB
-num_tests_max=10000
+max_mem=2 #GB
+num_tests_max=5000
 len_min=500
-len_max=`bc <<< "${max_mem}*1024*1024*1024"` # max length of 2 GB
+len_max=`bc <<< "${max_mem}*1024*1024*1024"`
 
 if [[ "${t}" == i32 ]] || [[ "${t}" == f32 ]]; then
   bytes=4
@@ -16,6 +16,6 @@ len=${len_min}
 while [ `bc <<< "${len}*${bytes}"` -lt ${len_max} ]; do
   fname=`printf %010d ${len}`
   echo Testing $t for N=$len...
-  src/test -n ${num_tests_max} -N ${len} -t ${t} -e 0.002 -csv > ${out_dir}/${fname}.csv
+  src/test -n ${num_tests_max} -N ${len} -t ${t} -e 0.0025 -csv > ${out_dir}/${fname}.csv
   len=`bc <<< "${len}*2"`
 done
