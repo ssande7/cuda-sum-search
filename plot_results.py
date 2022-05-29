43e092b689b9435f20a7a11c522c6d7355fe51f6
ssande7 <>
2022-05-29 17:16:37 +1000

    Update to plotting + minor cleanup

--- plot_results.py
+++ plot_results.py
@@ -12,22 +12,27 @@ def main():
     parser.add_argument('-x', '--exclude', type=int, nargs='+', default=[], help='Rows to exclude')
     parser.add_argument('-l', '--linear', action='store_true', help='Linear scale axes')
     parser.add_argument('-y', '--ymax', type=float, default=None, help='Maximum for y axis')
+    parser.add_argument('--xmin', type=float, default=None, help='Minimum for x axis')
     parser.add_argument('-o', '--output', type=str, help='output file')
+    parser.add_argument('--throughput', action='store_true', help='Y axis is throughput (time per item)')
+    parser.add_argument('--hide-conflicts', action='store_true', help='Hide "with conflicts" in legend')
+    parser.add_argument('--show-search', action='store_true', help='Show search type in legend')
     args = parser.parse_args()
 
     files = glob(f'{args.dir}/{args.type}/*.csv')
     files.sort()
 
-    N = [int(f.split('/')[-1].rstrip('.csv')) for f in files]
+    N = np.array([int(f.split('/')[-1].rstrip('.csv')) for f in files])
     data = [pd.read_csv(f, delimiter='\t+', index_col=False, engine='python') for f in files]
     nrows = len(data[0].index)
     plt.figure()
     ax = plt.axes()
     if not args.linear:
         ax.set_xscale('log')
-        ax.set_yscale('log')
+        # ax.set_yscale('log')
     colours = plt.cm.Paired(np.linspace(0,1,12))
-    colours = colours[[0, 1, 2, 3, 11, 5, 7, 8, 9],:]
+    colours = colours[[0, 0, 1, 1, 8, 5, 5, 5, 3, 3, 2, 9, 9],:]
+    formats = ['.--', '.-', '.--', '.-', '.-', '.:', '.--', '.-', '.--', '.-', '.-', '.--', '.-']
     for row in range(nrows):
         if row in args.exclude:
             # dummy plot to keep colours consistent
@@ -35,17 +40,26 @@ def main():
             continue
         times = np.array([d.iloc[row, 3] for d in data], dtype=np.float64)
         err = np.array([d.iloc[row, 4] for d in data], dtype=np.float64) # standard deviation as error bar
+        if args.throughput:
+            err /= times
+            times = N / times
+            err *= times
+        label = f'{data[0].iloc[row, 0].rstrip()}: {data[0].iloc[row, 1].rstrip()}.'
+        if args.show_search:
+            label += f' {data[0].iloc[row, 2].rstrip()} search.'
+        label = label.replace(': Linear', ': Linear scan').replace('efficient', 'efficient scan')
+        label = label.replace('Partial', 'Partial scan').replace('GPU Binary', 'Binary')
+        if args.hide_conflicts:
+            label = label.replace(' with conflicts', '')
         ax.errorbar(N, times,
                     yerr=err,
-                    fmt='.-',
-                    label=f'{data[0].iloc[row, 0].rstrip()}: {data[0].iloc[row, 1].rstrip()}. {data[0].iloc[row, 2].rstrip()} search.'
-                            .replace(': Linear', ': Linear scan')
-                            .replace('efficient', 'efficient scan')
-                            .replace('Partial', 'Partial scan'),
-                    color=colours[row,:]
+                    fmt=formats[row], color=colours[row,:],
+                    label=label,
                     )
     if args.ymax:
         plt.ylim(top=args.ymax, bottom=0)
+    if args.xmin:
+        plt.xlim(left=args.xmin)
     # xmark = 1024
     # _, xmax = plt.xlim()
     # ymin,ymax=plt.ylim()
@@ -54,7 +68,10 @@ def main():
     #     xmark *= 1024
     ax.legend()
     ax.set_xlabel('N')
-    ax.set_ylabel('Time (ns)')
+    if args.throughput:
+        ax.set_ylabel('Throughput (items per ns)')
+    else:
+        ax.set_ylabel('Time (ns)')
     ax.set_title(f'Sum and search time for {args.type}')
     plt.grid()
     if args.output:
--- src/scan_config.cuh
+++ src/scan_config.cuh
@@ -19,11 +19,11 @@ constexpr size_t CONFLICT_FREE_OFFSET(const size_t n);
 
 template<> __host__ __device__
 constexpr size_t CONFLICT_FREE_OFFSET<4>(const size_t n) {
-   return n >> LOG_NUM_BANKS_4B; //((n) >> NUM_BANKS_4B + (n) >> (2 * LOG_NUM_BANKS_4B));
+   return n >> LOG_NUM_BANKS_4B;
 }
 template<> __host__ __device__
 constexpr size_t CONFLICT_FREE_OFFSET<8>(const size_t n) {
-   return n >> LOG_NUM_BANKS_8B; //((n) >> NUM_BANKS_8B + (n) >> (2 * LOG_NUM_BANKS_8B));
+   return n >> LOG_NUM_BANKS_8B;
 }
 
 template<size_t bytes> __host__ __device__
